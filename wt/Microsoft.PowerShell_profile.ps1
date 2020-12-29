861fe6a80a3aef07cd1d90097d7ce33081ec4dbb
vechro <stenmr@gmail.com>
2020-12-29 12:15:55 +0200

    Add glow markdown rendering and open function

--- wt/Microsoft.PowerShell_profile.ps1
+++ wt/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,14 @@
+
+# Tests if given command exists
+# Adapted from https://devblogs.microsoft.com/scripting/use-a-powershell-function-to-see-if-a-command-exists/
+Function Test-CommandExists ([String] $command) {
+  $oldPreference = $ErrorActionPreference
+  $ErrorActionPreference = 'stop'
+  Try { if (Get-Command $command) { $true } }
+  Catch { $false }
+  Finally { $ErrorActionPreference = $oldPreference }
+}
+
 Function dial {
   Invoke-Expression "sshfs-win svc \sshfs.kr\root@vech.ro!22 X:"
 }
@@ -13,15 +24,41 @@ Function wex {
   Invoke-Expression "iex.bat --werl -S mix"
 }
 
-# Prints formatted documentation for any Elixir function either in the console or the browser
-Function exh ([String] $Name = "h", [Switch] $UseBrowser) {
-  if ($UseBrowser.IsPresent) {
-    $text = Invoke-Expression "elixir -e 'import IEx.Helpers; h $Name'" | Out-String | Show-Markdown -UseBrowser
+# Defines markdown rendered Elixir help depending on if you have glow installed or not
+if (Test-CommandExists "glow") {
+  # Prints documentation formatted with Glow for any Elixir function either in the console or the browser
+  Function exh ([String] $Name = "h", [Switch] $UseBrowser) {
+    if ($UseBrowser.IsPresent) {
+      Invoke-Expression "elixir -e 'import IEx.Helpers; h $Name'" | Out-String | Show-Markdown -UseBrowser
+    }
+    else {
+      Invoke-Expression "elixir -e 'import IEx.Helpers; h $Name'" | Out-String | glow - -w 70
+    }
   }
-  else {
-    $text = Invoke-Expression "elixir -e 'import IEx.Helpers; h $Name'" | Out-String -Stream
-    $text.Split("\n") | ForEach-Object -Process { $_.Trim() } | Out-String | Show-Markdown
+}
+else {
+  # Prints documentation formatted with native Powershell functions for any Elixir function either in the console or the browser
+  Function exh ([String] $Name = "h", [Switch] $UseBrowser) {
+    if ($UseBrowser.IsPresent) {
+      Invoke-Expression "elixir -e 'import IEx.Helpers; h $Name'" | Out-String | Show-Markdown -UseBrowser
+    }
+    else {
+      $text = Invoke-Expression "elixir -e 'import IEx.Helpers; h $Name'" | Out-String -Stream
+      $text.Split("\n") | ForEach-Object -Process { $_.Trim() } | Out-String | Show-Markdown
+    }
   }
+}
+
+
+
+# Open given directory in explorer, defaults to current directory (pipelineable)
+Function open {
+  [CmdletBinding()]
+  Param(
+    [Parameter(ValueFromPipeline)]
+    [String] $Dir = "."
+  )
+  Split-Path -Path $Dir | Invoke-Item
 }
 
 # Open current directory in explorer
@@ -63,117 +100,6 @@ function Write-PromptSegment {
   }
 
   Write-Host $Text -NoNewline -BackgroundColor $Background -ForegroundColor $Foreground
-
-  $script:bg = $Background;
-}
-
-function Get-FancyDir {
-  return $(Get-Location).ToString().Replace($env:USERPROFILE, '~').Replace('\', '  ');
-}
-
-function Get-GitBranch {
-  $HEAD = Get-Content $(Join-Path $(Get-GitDirectory) HEAD)
-  if ($HEAD -like 'ref: refs/heads/*') {
-    return $HEAD -replace 'ref: refs/heads/(.*?)', "$1";
-  }
-  else {
-    return $HEAD.Substring(0, 8);
-  }
-}
-
-function Write-PromptStatus {
-  if ($script:last) {
-    Write-PromptSegment ' ✅ ' Green Black
-  }
-  else {
-    Write-PromptSegment " ❌ $lastexitcode " Red White
-  }
-}
-
-function Write-PromptUser {
-  if ($global:admin) {
-    Write-PromptSegment ' # ADMIN ' Magenta White;
-  }
-  else {
-    Write-PromptSegment " $env:USERNAME " Yellow White;
-  }
-}
-
-function Write-PromptVirtualEnv {
-  if ($env:VIRTUAL_ENV) {
-    Write-PromptSegment " $(split-path $env:VIRTUAL_ENV -leaf) " Cyan Black
-  }
-}
-
-function Write-PromptDir {
-  Write-PromptSegment " $(Get-FancyDir) " DarkYellow White
-}
-
-# Depends on posh-git
-function Write-PromptGit {
-  if (Get-GitDirectory) {
-    Write-PromptSegment "  $(Get-GitBranch) " Blue White
-  }
-}
-
-function Reference {
-  # Powerline character reference
-  Write-Host "                                     "
-}
-
-function prompt {
-  $script:last = $?;
-  $script:first = $true;
-
-  # Write-Host "$(((H)[-1].EndExecutionTime - (H)[-1].StartExecutionTime).Milliseconds) ms" -NoNewline -ForegroundColor Gray
-
-  Write-PromptVirtualEnv
-  Write-PromptDir
-  Write-PromptGit
-
-  Write-PromptFancyEnd
-
-  return ' '
-}Function dial {
-  Invoke-Expression "sshfs-win svc \sshfs.kr\root@vech.ro!22 X:"
-}
-
-# Open current directory in explorer
-Set-PSReadLineKeyHandler -Key Ctrl+e -ScriptBlock { Invoke-Item . }
-
-Import-Module posh-git
-
-$script:bg = [Console]::BackgroundColor;
-$script:first = $true;
-$script:last = 0;
-
-function Write-PromptFancyEnd {
-  Write-Host  -NoNewline -ForegroundColor $script:bg
-
-  $script:bg = [System.ConsoleColor]::Black
-}
-
-function Write-PromptSegment {
-  param(
-    [Parameter(
-      Position = 0,
-      Mandatory = $true,
-      ValueFromPipeline = $true,
-      ValueFromPipelineByPropertyName = $true
-    )][string]$Text,
-
-    [Parameter(Position = 1)][System.ConsoleColor] $Background = [Console]::BackgroundColor,
-    [Parameter(Position = 2)][System.ConsoleColor] $Foreground = [System.ConsoleColor]::White
-  )
-
-  if (!$script:first) {
-    Write-Host  -NoNewline -BackgroundColor $Background -ForegroundColor $script:bg
-  }
-  else {
-    $script:first = $false
-  }
-
-  Write-Host $text -NoNewline -BackgroundColor $Background -ForegroundColor $Foreground
 
   $script:bg = $Background;
 }
